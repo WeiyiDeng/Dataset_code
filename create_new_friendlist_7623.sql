32c078c274f652eca301738df39163bb71a986ee
WeiyiDeng <weiyiwdeng@gmail.com>
2017-03-10 18:10:11 +0100

    strict adoption matrix prep and trends

---
+++ create_new_friendlist_7623.sql
@@ -0,0 +1,7 @@
+﻿create table new_friendlist_7623 (MEMBER integer, FRIEND integer);
+insert into new_friendlist_7623 (MEMBER, FRIEND)
+
+SELECT *
+FROM (select * from friendlist
+where MEMBER in (select distinct USER_ID from bandadoptions_strict_adopt)) as M
+WHERE FRIEND in (select distinct USER_ID from bandadoptions_strict_adopt)
--- test2.sql
+++ test2.sql
@@ -1,1 +1,24 @@
-﻿select count(distinct var2) from mat1
+﻿--select count(distinct BAND_ID) from modelbandweeks
+
+--select count(distinct USER_ID) from bandadoptions_strict_adopt
+
+--select count(distinct USER) from fbandslisn
+
+--select count(*) from fbandslisn
+
+--select count(*) from bandadoptions
+
+--select distinct USER_ID from bandadoptions_strict_adopt
+
+--select count(*) from peak_strict
+
+select USER_ID, mod_adoptions_strict.BAND_ID, mod_adoptions_strict.week_mod, introdate.week_mod as bandintro, peak_strict.week_mod as bandpeak
+from mod_adoptions_strict, peak_strict, introdate
+where mod_adoptions_strict.BAND_ID = peak_strict.BAND_ID and mod_adoptions_strict.BAND_ID = introdate.BAND_ID
+
+/*
+select count(*)
+from (SELECT *
+FROM (select * from friendlist
+where MEMBER in (select distinct USER_ID from bandadoptions_strict_adopt)) as M
+WHERE FRIEND in (select distinct USER_ID from bandadoptions_strict_adopt))
